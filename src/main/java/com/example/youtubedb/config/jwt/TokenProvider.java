eef1b59596351e49343c2fec2f58bb6d3494f630
JisooPark27 <jspark9812@gmail.com>
2021-09-07 02:16:12 +0900

    [Refactoring] 2차 리팩토링 - 지수

--- src/main/java/com/example/youtubedb/config/jwt/TokenProvider.java
+++ src/main/java/com/example/youtubedb/config/jwt/TokenProvider.java
@@ -1,6 +1,5 @@
 package com.example.youtubedb.config.jwt;
 
-import com.example.youtubedb.util.DateUtil;
 import com.example.youtubedb.domain.Token;
 import com.example.youtubedb.exception.NotExistAuthorityException;
 import io.jsonwebtoken.*;
@@ -19,10 +18,14 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.Period;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Date;
 import java.util.stream.Collectors;
+
+import static java.sql.Timestamp.valueOf;
 
 @Slf4j
 @Component
@@ -46,7 +49,6 @@ public class TokenProvider {
                 .collect(Collectors.joining(","));
 
         // Access Token 생성
-
         String accessToken = Jwts.builder()
                 .setSubject(authentication.getName())       // payload "sub": "name"
                 .claim(AUTHORITIES_KEY, authorities)        // payload "auth": "ROLE_USER"
@@ -55,7 +57,6 @@ public class TokenProvider {
                 .compact();
 
         // Refresh Token 생성
-
         String refreshToken = Jwts.builder()
                 .setExpiration(DateUtil.getRefreshTokenExpiresIn(isPC))
                 .signWith(key, SignatureAlgorithm.HS512)
@@ -99,5 +100,19 @@ public class TokenProvider {
 
     private Claims parseClaims(String accessToken) throws ExpiredJwtException {
         return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+    }
+
+    private static class DateUtil{
+        private static final Duration ACCESS_TOKEN_EXPIRE_TIME = Duration.ofMinutes(30);
+        private static final Period REFRESH_TOKEN_EXPIRE_DATE_APP = Period.ofDays(7);
+        private static final Period REFRESH_TOKEN_EXPIRE_DATE_PC = Period.ofMonths(3);
+
+        public static Date getAccessTokenExpiresIn(){
+            return valueOf(LocalDateTime.now().plus(ACCESS_TOKEN_EXPIRE_TIME));
+        }
+
+        public static Date getRefreshTokenExpiresIn(boolean isPC){
+            return valueOf(LocalDateTime.now().plus(isPC ? REFRESH_TOKEN_EXPIRE_DATE_PC : REFRESH_TOKEN_EXPIRE_DATE_APP));
+        }
     }
 }
