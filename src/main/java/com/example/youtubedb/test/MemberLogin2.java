01f583aed5ebe0bffffcb554155cdf709ed60dff
JisooPark27 <jspark9812@gmail.com>
2021-10-11 12:33:29 +0900

    feat : memberLogin test - isCorrectPassword SUCCESS

--- src/main/java/com/example/youtubedb/test/MemberLogin2.java
+++ src/main/java/com/example/youtubedb/test/MemberLogin2.java
@@ -1,5 +1,6 @@
 package com.example.youtubedb.test;
 
+import com.example.youtubedb.exception.DoNotMatchPasswordException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -8,6 +9,15 @@ public class MemberLogin2 {
   private final MemberRepository2 repository;
 
   public Member2 login(String loginId, String password){
-    return repository.find(loginId);
+    Member2 member = repository.find(loginId);
+
+    checkPassword(member, password);
+    return member;
+  }
+
+  private void checkPassword(Member2 member, String password) {
+    if(!member.password().equals(password)){
+      throw new DoNotMatchPasswordException();
+    }
   }
 }
