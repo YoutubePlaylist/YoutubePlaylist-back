6046bf9f836212952a5b0c4077a1a243add00b7e
JisooPark27 <jspark9812@gmail.com>
2021-10-11 12:42:31 +0900

    feat : memberLogin Test - isEncryptedPassword

--- src/test/java/com/example/youtubedb/test/MemberLogin2Test.java
+++ src/test/java/com/example/youtubedb/test/MemberLogin2Test.java
@@ -65,4 +65,23 @@ class MemberLogin2Test {
     assertThrows(DoNotMatchPasswordException.class, () -> memberLogin2.login("loginId", "password2"));
   }
 
+  PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+  @Test
+  @DisplayName("비밀번호가 암호화 되어 있는 비밀번호를 잘 확인하니")
+  void isEncryptedPassword() {
+    //given
+    Member2 member = new Member2("loginId", passwordEncoder.encode("password"));
+    repository.save(member);
+    repository.save(new Member2("loginId1", passwordEncoder.encode("password1")));
+    repository.save(new Member2("loginId2", passwordEncoder.encode("password2")));
+
+    //when
+    Member2 loginMember = memberLogin2.login("loginId", "password");
+
+    //then
+    assertThat(repository.find(member.loginId()), is(loginMember));
+    assertThat(repository.find(member.loginId()).password(), is(loginMember.password()));
+  }
+
 }
